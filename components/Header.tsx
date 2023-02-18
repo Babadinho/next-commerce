cba1c3da7bc85365ce8aeae71494fc355357ef5c
Donald Iloekwe <donytex4u@gmail.com>
2023-02-18 20:18:59 +0000

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='font-bold text-lg'>
+        <p className='font-bold text-lg'>Next Commerce</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
