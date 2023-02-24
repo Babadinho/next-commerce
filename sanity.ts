ad88ee820769fdd6f4177268387d5ddf59553569
Donald Iloekwe <donytex4u@gmail.com>
2023-02-24 16:19:08 +0000

    fix error causing fail for vercel deployment

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import createImageUrlBuilder from '@sanity/image-url';
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   token: process.env.SANITY_API_TOKEN,
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'lfrz4r27',
   apiVersion: '2021-08-11', // or today's date for latest,
   useCdn: process.env.NODE_ENV === 'production',
 };
