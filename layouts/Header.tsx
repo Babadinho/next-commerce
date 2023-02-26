58472fcc988c5daabca78b390aece60e301bc593
Donald Iloekwe <donytex4u@gmail.com>
2023-02-26 00:17:03 +0000

    style: remove header top margin for mobile

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <section className='mx-auto mt-6 flex h-[350px] md:h-[400px] max-w-[1250px] items-center justify-between bg-[#f7f7f7] px-4 md:px-20 shadow-sm border border-gray-200'>
+    <section className='mx-auto mt-0 md:mt-6 flex h-[350px] md:h-[400px] max-w-[1250px] items-center justify-between bg-[#f7f7f7] px-4 md:px-20 shadow-sm border border-gray-200'>
       <div className='space-y-5'>
         <h1 className='space-y-2 md:space-y-3 font-semibold tracking-wide'>
           <span className='block bg-gradient-to-t from-violet-700 to-pink-600 bg-clip-text text-transparent text-4xl lg:text-6xl'>
